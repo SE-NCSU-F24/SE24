abf17eec50f7a15752dfa4daaf065007f0d090f2
Karan Joshi <karanforprogramming@gmail.com>
2024-10-09 19:31:23 -0400

    Added task3.sh

---
+++ hw4/task3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+awk -F ',' '
+    $3 == 2 && $NF ~ /S/ {
+        gsub(/\<male\>/, "M", $0); 
+        gsub(/\<female\>/, "F", $0); 
+        if ($7 != "") {sum += $7; count++} 
+    } 
+    END {
+        if (count > 0) print "Average Age:", sum / count; 
+        else print "No valid ages found."
+    }
+' titanic.csv
