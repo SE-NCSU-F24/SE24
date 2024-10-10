c1d035ce527083337e3fe74a512b8945868ed045
Karan Joshi <karanforprogramming@gmail.com>
2024-10-10 14:53:44 -0400

    Fixed an error

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-grep ',2,' titanic.csv | grep 'S$' | \
-sed 's/<male>/M/g; s/<female>/F/g' | \
+cat titanic.csv | \
 awk -F ',' '
-    {
-        print $0;  # Print the entire row
-        if ($7 != "") {sum += $7; count++} 
-    } 
-    END {
-        if (count > 0) print "Average Age:", sum / count; 
-        else print "No valid ages found."
-    }'
+BEGIN {OFS=","}
+$3 == 2 && $NF ~ /S/ {
+    gsub(/\<male\>/, "M", $0)
+    gsub(/\<female\>/, "F", $0)
+    print $0
+    if ($7 != "") {sum += $7; count++}
+}
+END {
+    if (count > 0) print "Average Age:", sum / count
+    else print "No valid ages found."
+}' | \
+tee >(grep -v "Average Age:" > filtered_data.csv)
