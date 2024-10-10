9ca7c578194754244c50c05547ee972bd2b5cd18
Karan Joshi <karanforprogramming@gmail.com>
2024-10-10 13:35:13 -0400

    Fixed an error

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,24 +1,10 @@
-# #!/bin/bash
-
-# awk -F ',' '
-#     $3 == 2 && $NF ~ /S/ {
-#         gsub(/\<male\>/, "M", $0); 
-#         gsub(/\<female\>/, "F", $0); 
-#         if ($7 != "") {sum += $7; count++} 
-#     } 
-#     END {
-#         if (count > 0) print "Average Age:", sum / count; 
-#         else print "No valid ages found."
-#     }
-# ' titanic.csv
-
 #!/bin/bash
 
 awk -F ',' '
     $3 == 2 && $NF ~ /S/ {
         gsub(/\<male\>/, "M", $0); 
         gsub(/\<female\>/, "F", $0); 
-        print $4;  # Print the name (assuming the name is in the 2nd column)
+        print $4;
         if ($7 != "") {sum += $7; count++} 
     } 
     END {
